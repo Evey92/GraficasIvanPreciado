abd5b399932576528185b230ccfafe8e062e9c6e
Ivan <idv16c.ipreciado@uartesdigitales.edu.mx>
2018-03-12 17:09:12 -0600

    +agregué mas cosas

---
+++ My_IDV_Gfx/Project/Bin/Shaders/FS_Mesh.glsl
@@ -0,0 +1,12 @@
+uniform highp mat4 WVP;
+uniform highp mat4 World;
+uniform highp mat4 WorldView;
+
+varying highp vec2 vecUVCoords;
+varying highp vec3 Norm;
+varying highp vec4 Pos;
+
+void main(){
+	gl_FragColor = vec4(Norm, 1.0f);
+}
+
---
+++ My_IDV_Gfx/Project/Bin/Shaders/VS_Mesh.glsl
@@ -0,0 +1,19 @@
+attribute highp vec4 Vertex;
+attribute highp vec4 Normal; 
+attribute highp vec2 UV;
+
+
+varying highp vec2 vecUVCoords;
+varying highp vec3 Norm;
+varying highp vec4 Pos;
+
+uniform highp mat4 WVP;
+uniform highp mat4 World;
+uniform highp mat4 WorldView;
+
+void main(){
+	vecUVCoords = UV;	
+	Norm = Normal.xyz;
+	Pos = WVP*Vertex;
+	gl_Position = Pos;
+}
